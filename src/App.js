3ab2d19db1ee29d9a58de6f4f00492aa52b75864
beydtha <xbeydtha@gmail.com>
2018-04-15 02:01:08 +0200

    first Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+import Game from "./component/Game";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+          <Game />
+      
+      </div>
+    );
+  }
+}
+
+export default App;
